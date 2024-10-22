b0dc2f695f49b7c9929d8b8a2a3d8efcdf777e65
assad19 <abraham@falconpressurewash.com>
2024-10-22 12:40:58 +0500

    Quote Form Submission

--- js/quote.js
+++ js/quote.js
@@ -89,7 +89,7 @@ services.forEach(service => {
     service.addEventListener('click', function() {
         const serviceName = service.getAttribute('title');
         if (!selectedServices.includes(serviceName)) {
-            selectedServices.push(serviceName);
+            selectedServices.push({name:serviceName});
             service.style.border = '2px solid green'; // Mark selected
         } else {
             selectedServices = selectedServices.filter(s => s !== serviceName);
@@ -161,41 +161,41 @@ document.getElementById('quoteForm').addEventListener('submit', function(event) 
         document.getElementById('serviceError').style.display = 'block';
     }
 
+    var recaptchaResponse = grecaptcha.getResponse();
+    if (recaptchaResponse.length === 0) {
+        alert('Please complete the reCAPTCHA.');
+        isValid = false;
+    }
+
     // Prevent form submission if not valid
     if (!isValid) {
         event.preventDefault(); // Prevent form submission
     } 
 
-    var recaptchaResponse = grecaptcha.getResponse();
-    if (recaptchaResponse.length === 0) {
-        alert('Please complete the reCAPTCHA.');
-        return;
-    }
     else {
-        console.log(firstName.value, lastName.value, email, phone, address.value, city.value, state.value, zipCode, source, selectedServices, promoCode.value, comments.value);
-        emailjs.send("service_equq3ns", "template_zrlwa2n", {
-            firstName:firstName.value, 
-            lastName:lastName.value, 
-            email:email, 
-            phone:phone, 
-            address:address.value, 
-            city:city.value, 
-            state:state.value, 
-            zipCode:zipCode, 
-            source:source, 
-            selected:selectedServices, 
-            promo:promoCode.value, 
-            comments:comments.value,
-            "g-recaptcha-response": recaptchaResponse // Pass the reCAPTCHA response to EmailJS (if needed)
-          }).then(function(response) {
-            console.log('SUCCESS!', response.status, response.text);
-            alert('Thanks for the submission. We will reply to you shortly.');
-            location.reload();
-          }, function(error) {
-            // console.log('FAILED...', error);
-            alert('Something went wrong. Please try again. Error: ' + error.text);
-            location.reload();
-          });
+        console.log(firstName.value, lastName.value, email, phone, address.value, city.value, state.value, zipCode, source, selectedServices.name, promoCode.value, comments.value);
+        // emailjs.send("service_equq3ns", "template_l7n63xe", {
+        //     firstName:firstName.value, 
+        //     lastName:lastName.value, 
+        //     email:email, 
+        //     phone:phone, 
+        //     address:address.value, 
+        //     city:city.value, 
+        //     state:state.value, 
+        //     zipCode:zipCode, 
+        //     source:source, 
+        //     selected:selectedServices.name, 
+        //     promo:promoCode.value, 
+        //     comments:comments.value
+        //   }).then(function(response) {
+        //     console.log('SUCCESS!', response.status, response.text);
+        //     alert('Thanks for the submission. We will reply to you shortly.');
+        //     location.reload();
+        //   }, function(error) {
+        //     // console.log('FAILED...', error);
+        //     alert('Something went wrong. Please try again. Error: ' + error.text);
+        //     location.reload();
+        //   });
     }
 });
 
