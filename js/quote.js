043ebb98ed0c6e5308c0dea90e08cc0b99ef6916
assad19 <abraham@falconpressurewash.com>
2024-10-22 13:22:54 +0500

    Recap Parameter

--- js/quote.js
+++ js/quote.js
@@ -196,7 +196,8 @@ document.getElementById('quoteForm').addEventListener('submit', function(event) 
             source:source, 
             selected:fetchServices(), 
             promo:promoCode.value, 
-            comments:comments.value
+            comments:comments.value,
+            "g-recaptcha-response": recaptchaResponse
           }).then(function(response) {
             // console.log('SUCCESS!', response.status, response.text);
             alert('Thanks for the submission. We will reply to you shortly.');
