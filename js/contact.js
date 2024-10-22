5df667b4c14c3e41c12f1e3cf0a8f15ad207e69f
assad19 <abraham@falconpressurewash.com>
2024-10-22 11:39:37 +0500

    Homepage Form Validation

--- js/contact.js
+++ js/contact.js
@@ -1,23 +1,3 @@
-//  console.log("added")
-
-//  document.getElementById('contactForm').addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     // Send email using EmailJS
-//     emailjs.send("service_equq3ns", "template_zrlwa2n", {
-//       name: document.getElementById('name').value,
-//       email: document.getElementById('email').value,
-//       phone: document.getElementById('phone').value,
-//       message: document.getElementById('message').value
-//     }).then(function(response) {
-//       alert('Thanks for the submission. We will reply you shortly');
-//       console.log(event)
-//     }, function(error) {
-//       alert('Something went wrong, Please try again ' + error.text);
-//     });
-//   });
-
-
 console.log("Contact form script added!");
 document.getElementById('contactForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent default form submission behavior
