3fa894dea61516b09f41546b89ca2c4703393260
assad19 <abraham@falconpressurewash.com>
2024-10-22 10:51:23 +0500

    Contact Form Email Validation

--- js/contact.js
+++ js/contact.js
@@ -3,13 +3,15 @@
     event.preventDefault();
 
     // Send email using EmailJS
-    emailjs.send("service_cxjwvxg", "template_ytnf5q3", {
+    emailjs.send("service_equq3ns", "template_zrlwa2n", {
       name: document.getElementById('name').value,
       email: document.getElementById('email').value,
+      phone: document.getElementById('phone').value,
       message: document.getElementById('message').value
     }).then(function(response) {
       alert('Thanks for the submission. We will reply you shortly');
+      console.log(event)
     }, function(error) {
-      alert('Something went wrong' + error.text);
+      alert('Something went wrong, Please try again' + error.text);
     });
   });
